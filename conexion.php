0da5bea239130cdbf922b1a1c50e9ef665b7a333
Abel Benjamín Martínez Solano <103260147+AbelSolano@users.noreply.github.com>
2022-05-28 17:03:31 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,1 +1,26 @@
+<?php
+
+    $conexion = mysqli_connect('localhost','root','','marsol')
+    or die(mysqli_error($mysqli));
+
+    insertar($conexion);
+    
+    
+    function insertar($conexion){
+        $usuario =$_POST['usuario'];
+        $nombre =$_POST['nombre'];
+        $password=$_POST['password'];
+        $correo =$_POST['correo'];
+        $telefono=$_POST['telefono'];
+        
+        $id= $_POST['id'];
+
+        $consulta ="INSERT INTO usuarios (usuario, nombre, password, correo, telefono,id) 
+        VALUES ('$usuario', '$nombre', '$password','$correo','$telefono','$id')";
+        mysqli_query($conexion, $consulta);
+        mysqli_close($conexion);
+        header("Location: crearcuenta2.php");
+        }    
+         
+?>
 
