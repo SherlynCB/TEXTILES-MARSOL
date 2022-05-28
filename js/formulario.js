3c6c4c3eb50196f4ab0e5a5aa53d3fc825a7dff0
Abel Benjamín Martínez Solano <103260147+AbelSolano@users.noreply.github.com>
2022-05-28 17:39:04 -0500

    Update formulario.js

--- js/formulario.js
+++ js/formulario.js
@@ -8,6 +8,7 @@ const expresiones = {
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
 }
+
 const campos = {
 	usuario: false,
 	nombre: false,
