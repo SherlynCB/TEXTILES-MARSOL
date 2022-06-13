08099d525f5add1dbde819c20fd8dadf1f0eec6c
Sherlyn Cruz Bautista <104521850+SherlynCB@users.noreply.github.com>
2022-06-12 19:00:14 -0500

    Update formulario.js

--- js/formulario.js
+++ js/formulario.js
@@ -85,25 +85,25 @@ inputs.forEach((input) => {
 	input.addEventListener('blur', validarFormulario);
 });
 
-formulario.addEventListener('submit', (e) => {
-	e.preventDefault();
+// formulario.addEventListener('submit', (e) => {
+// 	e.preventDefault();
 
-	const terminos = document.getElementById('terminos');
-	if(campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked ){
-		formulario.reset();
+// 	const terminos = document.getElementById('terminos');
+// 	if(campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked ){
+// 		formulario.reset();
 
-		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
-		setTimeout(() => {
-			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
-		}, 5000);
+// 		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+// 		setTimeout(() => {
+// 			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+// 		}, 5000);
 
-		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
-			icono.classList.remove('formulario__grupo-correcto');
-		});
-	} else {
-		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
-		setTimeout(() => {
-			document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
-		}, 5000);
-	}
-});
+// 		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+// 			icono.classList.remove('formulario__grupo-correcto');
+// 		});
+// 	} else {
+// 		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+// 		setTimeout(() => {
+// 			document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
+// 		}, 5000);
+// 	}
+// });
